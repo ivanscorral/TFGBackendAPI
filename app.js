a1f4f196f4daeb35e6a00150752aad20176ec7a1
Iván Sánchez Corral <ramon.sanchez68@gmail.com>
2022-07-27 16:39:05 +0200

    Add some routes as example for later

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth');
+var actionsRouter = require('./routes/actions');
 
 var app = express();
 
@@ -16,5 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/action', actionsRouter);
 
 module.exports = app;
--- routes/actions.js
+++ routes/actions.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('action');
 });
 
+router.get('/reading', function(req, res, next) {
+  res.send('set reading to x');
+});
+
 module.exports = router;
